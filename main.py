0677f60e53430adbf209f697344d7f9a27d7fc57
Heridaspollux <34633555+Heridaspollux@users.noreply.github.com>
2022-12-12 02:00:51 +0100

    Update main.py
    1: I added def heat_map( please check in the input of def eval_routine. I dont know what they are)
    2: I added plot( we need to recall for plot)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from evaluation import get_song_genre, relevance, recall, prec, eval_routine, ge
     plot_prec_rec, pairwise_corr, corr_matrix
 from ast import literal_eval
 from sklearn.metrics import PrecisionRecallDisplay
+import matplotlib.pyplot as plt
 
 # data import
 genres = pd.read_csv('./data/id_genres_mmsr.tsv', delimiter='\t', index_col="id")
@@ -57,6 +58,87 @@ print(corr_matrix(baseline, m1, m2, m3, m4, m5))
 with open("sample_1000.txt", 'r', encoding="utf-8") as f:
     subset1000 = [line.rstrip('\n') for line in f]
 
+    
+def heat_map(query_set,top_k):
+    ap_Base, mrr_Base, ndcg_Base = eval_routine(query_set, baseline['cos_sim'], df_song_info, top_k)
+    ap_M1, mrr_M1, ndcg_M1 = eval_routine(query_set, m1['cos_sim'], df_song_info, top_k)
+    ap_M2, mrr_M2, ndcg_M2 = eval_routine(query_set, m2['cos_sim'], df_song_info, top_k)
+    ap_M3, mrr_M3, ndcg_M3 = eval_routine(query_set, m3['cos_sim'], df_song_info, top_k)
+    ap_M4, mrr_M4, ndcg_M4 = eval_routine(query_set, m4['cos_sim'], df_song_info, top_k)
+    ap_M5, mrr_M5, ndcg_M5 = eval_routine(query_set, m5['cos_sim'], df_song_info, top_k)
+    
+    # Heat map
+
+    average = ["AP", "MRR", "leCG"]
+    models = ["TF-IDF", "BERT", "BLF Spectral","BLF Correlation", "ResNet", "VGG19"]
+
+
+    arr_base = [ap_Base, mrr_Base, ndcg_Base]
+    arr_m1   = [ap_M1, mrr_M1, ndcg_M1]
+    arr_m2   = [ap_M2, mrr_M2, ndcg_M2]
+    arr_m3   = [ap_M3, mrr_M3, ndcg_M3]
+    arr_m4   = [ap_M4, mrr_M4, ndcg_M4]
+    arr_m5   = [ap_M5, mrr_M5, ndcg_M5]
+
+    heat = np.array([arr_base,arr_m1,arr_m2,arr_m3,arr_m4,arr_m5])
+
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(heat)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(models)), labels=models)
+    ax.set_yticks(np.arange(len(average)), labels=average)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(average)):
+        for j in range(len(models)):
+            text = ax.text(j, i, heat[i, j], ha="center", va="center", color="w")
+
+    ax.set_title("Evaluation Heat Map")
+    fig.tight_layout()
+    plt.show()   
+    
+    
+    
+    
+heat_map("Can You Feel My Heart",10)
+heat_map("Can You Feel My Heart",100)
+    
+ 
+
+
+# main format : plt.plot(recall_baseline, precision_baseline, 'r')
+#These are only for test and show, We should change them
+
+plt.plot(m3['cos_sim'], baseline['cos_sim'], 'r')
+plt.plot(m4['cos_sim'], m2['cos_sim'], 'b')
+plt.plot(m5['cos_sim'], m2['cos_sim'], 'g')
+plt.plot(m3['cos_sim'], m2['cos_sim'], 'y')
+plt.plot(m5['cos_sim'], m3['cos_sim'], 'm')
+plt.plot(m3['cos_sim'], m5['cos_sim'], 'c')
+
+plt.legend(['baseline', 'M1','M2', 'M3', 'M4', 'M5' ])
+
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision versus Recall')
+plt.show()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 # run evaluation routine
 #print("Using tf-idf")
